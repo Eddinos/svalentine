4d8fb2943c31a36ce03247c63226e850c6e99121
Eddine Djerboua <edjerboua-ext@lequipe.fr>
2020-04-17 19:40:08 +0200

    process.env obviously

--- functions/pictures.js
+++ functions/pictures.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 exports.handler = async function(event, context, callback) {
-    let { data } = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${FLICKER_KEY}&format=json&nojsoncallback=1&user_id=${FLCIKER_ACCOUNT_ID}&extras=original_format`)
+    let { data } = await axios.get(`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${process.env.FLICKER_KEY}&format=json&nojsoncallback=1&user_id=${process.env.FLCIKER_ACCOUNT_ID}&extras=original_format`)
         
     const photosList = data.photos.photo.map( ({farm, server, id, secret, originalsecret, originalformat}) => ({
         small: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_q.jpg`,
