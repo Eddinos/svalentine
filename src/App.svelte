55c0a067a1630b6f7764227842b0c63ff6e30bca
Eddine Djerboua <edjerboua-ext@lequipe.fr>
2020-04-19 14:43:07 +0200

    Fix index

--- src/App.svelte
+++ src/App.svelte
@@ -193,8 +193,8 @@
 	 out:send={{key: selected}} 
 	 on:click={closeDetails}>
 	<div class="details__content">
-		<img class="details__image" src="{postCards[selected - 1].srcLarge}" alt="">
-		<p class="details__text">{postCards[selected - 1].text}</p>
+		<img class="details__image" src="{postCards[selected].srcLarge}" alt="">
+		<p class="details__text">{postCards[selected - 1].text || ''}</p>
 	</div>
 	
 </div>
